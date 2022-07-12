f7b9a068509c8467eefef141d944aeabfeaac390
Ibitowa Ifeoluwa <ibitowaifeoluwa@yahoo.com>
2022-07-12 01:09:53 -0700

    The printf file and the header file

---
+++ _printf.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _printf - produces output according to a format.
+ * @format: Input character string.
+ * Return: The number of characters printed.
+ */
+
+int _printf(const char *format, ...)
+{
+	return (0);
+}
