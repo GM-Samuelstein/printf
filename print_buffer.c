9ba5c18f0e9b91030e3f6d4ab28655c88ec6a1ab
Ibitowa Ifeoluwa <ibitowaifeoluwa@yahoo.com>
2022-07-12 14:41:01 -0700

    Betty fix

--- print_buffer.c
+++ print_buffer.c
@@ -4,6 +4,7 @@
  * print_buffer - prints buffer.
  * @buf: pointer to buffer.
  * @nbuf: number of bytes to be printed.
+ * Return: number of bytes printed.
  */
 
 int print_buffer(char *buf, unsigned int nbuf)
