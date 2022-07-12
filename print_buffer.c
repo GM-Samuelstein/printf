a7978b737a221b41881d5f65e3f5fa85fcb9fcfb
Ibitowa Ifeoluwa <ibitowaifeoluwa@yahoo.com>
2022-07-12 13:19:56 -0700

    update

---
+++ print_buffer.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * print_buffer - prints buffer.
+ * @buf: pointer to buffer.
+ * @nbuf: number of bytes to be printed.
+ */
+
+int print_buffer(char *buf, unsigned int nbuf)
+{
+	return (write(1, buf, nbuf));
+}
